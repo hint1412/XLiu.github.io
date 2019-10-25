407d002550cff67e7bfbc243f7e6b54254a9cc81
hint1412 <57014059+hint1412@users.noreply.github.com>
2019-10-25 17:34:02 -0400

    Update main.js

--- SIF/js/main.js
+++ SIF/js/main.js
@@ -1,16 +1,50 @@
+/*
+variables
+*/
+var model;
+
+/*
+display after click
+*/
 function displayme(){
-var x = parseInt(document.getElementById("field1").value);
-var y = parseInt(document.getElementById("field2").value);
-var hammingDistance = function(x,y){
-    var count = 0;
-    var n = x ^ y ;
-    while ( n> 0 ){
-        if ((n&1)==1){
-            count += 1;
+    var x = parseInt(document.getElementById("field1").value);
+    var y = parseInt(document.getElementById("field2").value);
+    var sif = function(x,y){
+        var count = 0;
+        var n = x ^ y ;
+        while ( n> 0 ){
+            if ((n&1)==1){
+                count += 1;
+            }
+            n>>=1;
         }
-        n>>=1;
-    }
-    return count;
-};
-document.getElementById("output").value = hammingDistance(x,y);
+        return count;
+    };
+    document.getElementById("output").value = sif(x,y);
 }
+
+/*
+load the model
+*/
+async function start(cur_mode) { 
+    //load the model 
+    model = await tf.loadLayersModel('../models/model.json')
+    
+    //warm up 
+    model.predict(tf.zeros([2,]))
+    
+    //allow drawing on the canvas 
+    allowinput()
+    
+    //load the class names
+    //await loadDict()
+}
+
+/*
+allow input
+*/
+function allowinput() {
+    //document.getElementById('status').innerHTML = 'Model Loaded';
+    $('button').prop('disabled', false);
+}
+

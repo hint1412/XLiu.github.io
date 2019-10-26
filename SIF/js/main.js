0e41f4df03386dd850778ca10038d7c67e2ca65b
hint1412 <57014059+hint1412@users.noreply.github.com>
2019-10-25 20:59:52 -0400

    Update main.js

--- SIF/js/main.js
+++ SIF/js/main.js
@@ -10,15 +10,10 @@ function displayme(){
     var x = parseInt(document.getElementById("field1").value);
     var y = parseInt(document.getElementById("field2").value);
     var sif = function(x,y){
-        var count = 0;
-        var n = x ^ y ;
-        while ( n> 0 ){
-            if ((n&1)==1){
-                count += 1;
-            }
-            n>>=1;
+        var inputs = tf.tensor([[x, y]]);
+        var outputs = model.predict(inputs).dataSync();
         }
-        return count;
+        return outputs;
     };
     document.getElementById("output").value = sif(x,y);
 }
@@ -31,9 +26,9 @@ async function start() {
     model = await tf.loadLayersModel('models/model.json')
     
     //warm up 
-    const a = tf.tensor([[1, 2]]);
+    var a = tf.tensor([[1, 2]]);
     console.log('a shape:', a.shape, a.dtype);
-    const pred = model.predict(a).dataSync();
+    var pred = model.predict(a).dataSync();
     console.log('pred:', pred);
     
     //allow drawing on the canvas 

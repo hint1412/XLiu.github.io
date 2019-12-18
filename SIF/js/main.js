97ec708c9b495b384702cf94da92e653b178d597
hint1412 <57014059+hint1412@users.noreply.github.com>
2019-12-18 17:42:28 -0500

    Update main.js

--- SIF/js/main.js
+++ SIF/js/main.js
@@ -12,14 +12,15 @@ function displayme(){
     var w = parseFloat(document.getElementById("input_w").value);
     var l0 = parseFloat(document.getElementById("input_l0").value);
     var l1 = parseFloat(document.getElementById("input_l1").value);
-    var P = parseFloat(document.getElementById("input_P").value);
+    var P = parseFloat(document.getElementById("input_P").value);    
     var KP = function(a,b,w,l0,l1){
-        var inputs = tf.tensor([[a/b, w/b]]);
+        var inputs = tf.tensor([[a/b, w/b, l0/b, l1/b]]);
         var outputs = model.predict(inputs).dataSync();
-        return outputs;
+        var kop = outputs*l1*Math.pow(a, 0.5)/b/w/w;
+        return kop;
     };
     document.getElementById("output_KP").value = KP(a,b,w,l0,l1);
-    document.getElementById("output_K").value = 0.0;
+    document.getElementById("output_K").value = KP(a,b,w,l0,l1)*P;
 }
 
 /*
@@ -30,7 +31,7 @@ async function start() {
     model = await tf.loadLayersModel('models/model.json')
     
     //warm up 
-    var a = tf.tensor([[1, 2]]);
+    var a = tf.tensor([[1, 1, 1, 1]]);
     console.log('a shape:', a.shape, a.dtype);
     var pred = model.predict(a).dataSync();
     console.log('pred:', pred);

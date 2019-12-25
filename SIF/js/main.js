ba457aaa99ecd3a0bbc356884f3137acdc1322ec
Xing Liu <57014059+hint1412@users.noreply.github.com>
2019-12-24 20:40:58 -0500

    Add files via upload

--- SIF/js/main.js
+++ SIF/js/main.js
@@ -2,36 +2,76 @@
 variables
 */
 var model;
+var record_num = 0;
 
 /*
 display after click
 */
-function displayme(){
+function displayme() {
+    // record number
+    record_num += 1;
+    
+    // inputs
     var a = parseFloat(document.getElementById("input_a").value) || 0.0;
     var b = parseFloat(document.getElementById("input_b").value) || 0.0;
     var w = parseFloat(document.getElementById("input_w").value) || 0.0;
     var l0 = parseFloat(document.getElementById("input_l0").value) || 0.0;
     var l1 = parseFloat(document.getElementById("input_l1").value) || 0.0;
-    var P = parseFloat(document.getElementById("input_P").value) || 0.0;    
-    var KP = function(a,b,w,l0,l1){
+    var P = parseFloat(document.getElementById("input_P").value) || 0.0;
+    const elem_intp = document.getElementById("intp");
+    const elem_outk = document.getElementById("output_K");
+    const elem_recd = document.getElementById("txtarea");
+    
+    // evaluation    
+    var KP = function(a,b,w,l0,l1) {
         var inputs = tf.tensor([[a/b, w/b, l0/b, l1/b]]);
         var outputs = model.predict(inputs).dataSync();
-        var kop = outputs*l1*Math.pow(a, 0.5)/b/w/w;
-        return kop || 0.0;
+        var kop = (outputs*l1*Math.pow(a, 0.5)/b/w/w);       
+        return isFinite(kop) ? kop : 0.0;
     };
-    var KP_o = KP(a,b,w,l0,l1);
     var K_o = KP(a,b,w,l0,l1)*P;
-    document.getElementById("Intp").textContent = 'Extrapolation';    
-    if (a/b >= 0.1 && a/b <= 0.8) {
-        if (w/b >= 1.0 && w/b <= 3.0) {
-            if (l0/b >= 0.1 && l0/b <= 0.4) {
-                if (l1/b >= 2.0 && l1/b <= 5.0) {
-                    document.getElementById("Intp").textContent = 'Interpolation';                    
-                }
-            }
-        }        
+    
+    // log
+    var str_output = "a=" + a.toFixed(3).toString() + 
+                      ", b=" + b.toFixed(3).toString() +
+                      ", w=" + w.toFixed(3).toString() +
+                      ", L0=" + l0.toFixed(3).toString() +
+                      ", L1=" + l1.toFixed(3).toString() +
+                      ", P=" + P.toFixed(3).toString() +
+                      ", KI=" + K_o.toFixed(3).toString();
+    console.log(str_output);     
+                           
+    // outputs 
+    if (a/b >= 0.1 && a/b <= 0.8 && w/b >= 1.0 && w/b <= 3.0 &&
+        l0/b >= 0.1 && l0/b <= 0.4 && l1/b >= 2.0 && l1/b <= 5.0) {
+        elem_intp.innerHTML = '<i class="far fa-grin-wink"></i> \
+                               &nbsp; Interpolation';
+        elem_intp.classList = "btn btn-outline-success \
+                               btn-lg btn-block mb-3"; 
+        elem_outk.style.color = "black";
+        elem_recd.innerHTML += "Record " + pad(record_num, 3) + 
+                               ":  " + str_output + " (Interpolation)" +
+                               ";" + "&#13;&#10;";        
+    } else {
+        elem_intp.innerHTML = '<i class="far fa-frown"></i> \
+                               &nbsp; Extrapolation';
+        elem_intp.classList = "btn btn-outline-danger \
+                               btn-lg btn-block mb-3";
+        elem_outk.style.color = "#d9534f";
+        elem_recd.innerHTML += "Record " + pad(record_num, 3) + 
+                               ":  " + str_output + " (Extrapolation)" +
+                               ";" + "&#13;&#10;";          
     }
-    document.getElementById("output_K").value = K_o.toFixed(3);
+    elem_outk.value = K_o.toFixed(3);      
+}
+
+/*
+zero padding
+*/
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
 /*
@@ -47,15 +87,14 @@ async function start() {
     var pred = model.predict(a).dataSync();
     console.log('pred:', pred);
     
-    //allow drawing on the canvas 
-    allowinput()
+    //allow running
+    allowrun()
 }
 
 /*
 allow input
 */
-function allowinput() {
+function allowrun() {
     document.getElementById('status').innerHTML = 'Model Loaded';
-    document.getElementById("button").disabled = false;
-    //$('button').prop('disabled', false);
+    document.getElementById("run").disabled = false;
 }

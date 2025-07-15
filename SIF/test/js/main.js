35216c536333d92ee2a268f4775c26873c2d5d14
Xing Liu <hint1412@gmail.com>
2025-07-14 23:01:40 -0400

    Update main.js

--- SIF/test/js/main.js
+++ SIF/test/js/main.js
@@ -25,13 +25,13 @@ function displayme() {
     const elem_recd = document.getElementById("txtarea");
     
     // evaluation    
-    var KP = function(input_1, input_2, input_3, input_4, input_5, input_6, input_7, input_8, input_9) {
+    function KP(input_1, input_2, input_3, input_4, input_5, input_6, input_7, input_8, input_9) {
         var inputs = tf.tensor([[input_1, input_2/10.0, input_3/10.0, input_4*10.0, input_5*10.0, input_6/10.0, input_7/10.0, input_8/10.0, input_9/1000.0]]);
         var outputs = model.predict(inputs).dataSync();
         var kop = outputs;       
         return isFinite(kop) ? kop : 0.0;
     };
-    var K_o = KP(input_1, input_2, input_3, input_4, input_5, input_6, input_7, input_8, input_9)*1.0;
+    var K_o = KP(input_1, input_2, input_3, input_4, input_5, input_6, input_7, input_8, input_9);
     
     // log: C, Mn, Si, P, S, Cu, Ni, Cr
     var str_output = "C" + input_1.toFixed(3).toString() + 
@@ -43,7 +43,7 @@ function displayme() {
 		      ", Ni" + input_7.toFixed(3).toString() +
                       ", Cr" + input_8.toFixed(3).toString() +
                       ", T" + input_9.toFixed(3).toString() +
-                      ", KI=" + K_o.toString();
+                      ", KI=" + K_o.toFixed(3).toString();
     console.log(str_output);    
                            
     // outputs 
@@ -52,7 +52,7 @@ function displayme() {
                            ":  " + str_output + 
                            ";" + "&#13;&#10;";        
         
-    elem_outk.value = K_o;      
+    elem_outk.value = K_o.toFixed(3);      
 }
 
 /*
